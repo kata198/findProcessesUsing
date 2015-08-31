96ee6f1d9073bd985548ebc33e8c13a6d02a8faa
Tim Savannah <kata198@gmail.com>
2015-08-30 20:07:53 -0400

    Initial Checkin

--- README.rst
+++ README.rst
@@ -1,21 +1,31 @@
-# findProcessesUsing
+findProcessesUsing
+==================
+
 Scans all running applications on a host to identify those using a shared library, or an executable, or some other mapping.
 
 
 This application works on UNIX-derived systems (Linux, BSD, etc). You can use it, for example, to scan for processes using a certain version of a shared library, or running under a certain interpreter. It can print a summarized view, or optionally print all matching mappings as well.
+
 
 This could be paired with https://github.com/kata198/remote\_copy\_and\_execute to do audits of running software/library usage across many machines on a network.
 
 
 You must be root to scan all running processes, otherwise this will only scan that which is running under your current user.
 
+
 Example Usage
 =============
 
 	]$ sudo findProcessesUsing libc | head -n 20 | tail -n5
+
 	Found libc in 803 (john) [ -bash  ]
+
 	Found libc in 1060 (john) [ /usr/lib/tracker/tracker-extract  ]
+
 	Found libc in 1062 (www) [ /usr/bin/httpd  ]
+
 	Found libc in 808 (frankl) [ /bin/sh /usr/bin/startx  ]
+
 	Found libc in 1065 (frankl) [ /usr/lib/tracker/tracker-miner-user-guides  ]
+
 
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+
+if __name__ == '__main__':
+
+    try:
+        with open('README.rst', 'r') as f:
+            long_description = f.read()
+    except:
+        long_description = ''
+
+    setup(name='findProcessesUsing',
+            version='1.0',
+            author='Tim Savannah',
+            author_email='kata198@gmail.com',
+            maintainer='Tim Savannah',
+            scripts=['findProcessesUsing'],
+            install_requires=['ProcessMappingScanner'],
+            url='https://github.com/kata198/findProcessesUsing',
+            maintainer_email='kata198@gmail.com',
+            description='Application which scans running processes on the system for a given mapping (executable, shared object, etc)',
+            long_description=long_description,
+            license='LGPLv3',
+            keywords=['find', 'process', 'using', 'so', 'mapping', 'scanner', 'unix', 'proc', 'mappings', 'lib', 'detect', 'executable', 'shared', 'object'],
+            classifiers=['Development Status :: 5 - Production/Stable',
+                         'Programming Language :: Python',
+                         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                         'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 2.7',
+                          'Programming Language :: Python :: 3',
+                          'Programming Language :: Python :: 3.3',
+                          'Programming Language :: Python :: 3.4',
+            ]
+    )
+
+
+
